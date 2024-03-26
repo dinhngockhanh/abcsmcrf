0373f521c0d7ce324357dd19e8e75cf38b76c0c1
ZijinXiang <zx2406@columbia.edu>
2024-03-26 10:49:56 -0400

    updates on sfs

--- vignettes/sfs.r
+++ vignettes/sfs.r
@@ -1,223 +1,232 @@
-# # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Zijin - Macbook
-# # R_workplace <- "/Users/xiangzijin/Documents/ABC_SMCRF/0321_sfs"
-# # R_libPaths <- ""
-# # R_libPaths_extra <- "/Users/xiangzijin/SMC-RF/R"
-# # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Zijin - Macmini
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Zijin - Macbook
+R_workplace <- "/Users/xiangzijin/Documents/ABC_SMCRF/0321_sfs"
+R_libPaths <- ""
+R_libPaths_extra <- "/Users/xiangzijin/SMC-RF/R"
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Zijin - Macmini
 # R_workplace <- "/Users/khanhngocdinh/Documents/Zijin/0322_sfs_experiments"
 # R_libPaths <- ""
 # R_libPaths_extra <- "/Users/khanhngocdinh/Documents/Zijin/SMC-RF/R"
-# # =======================================SET UP FOLDER PATHS & LIBRARIES
-# # devtools::install_github("rdinnager/slimr", force = TRUE)
-# # slim_setup()
-# # usethis::edit_r_environ() SLIM_PATH="/Library/Frameworks/R.framework/Versions/4.3-arm64/Resources/library/slimr"
-# # Sys.setenv(SLIM_PATH = "/Library/Frameworks/R.framework/Versions/4.3-arm64/Resources/library/slimr")
-# .libPaths(R_libPaths)
-# library(ggplot2)
-# library(gridExtra)
-# library(grid)
-# library(invgamma)
-# setwd(R_libPaths_extra)
-# files_sources <- list.files(pattern = "\\.[rR]$")
-# sapply(files_sources, source)
-# setwd(R_workplace)
+# =======================================SET UP FOLDER PATHS & LIBRARIES
+# devtools::install_github("rdinnager/slimr", force = TRUE)
+# slim_setup()
+# usethis::edit_r_environ() SLIM_PATH="/Library/Frameworks/R.framework/Versions/4.3-arm64/Resources/library/slimr"
+# Sys.setenv(SLIM_PATH = "/Library/Frameworks/R.framework/Versions/4.3-arm64/Resources/library/slimr")
+.libPaths(R_libPaths)
+library(ggplot2)
+library(gridExtra)
+library(grid)
+library(invgamma)
+setwd(R_libPaths_extra)
+files_sources <- list.files(pattern = "\\.[rR]$")
+sapply(files_sources, source)
+setwd(R_workplace)
 
-# # ===========================Model for the Site Frequency Spectrum (SFS)
-# #   Input:  data frame of parameters, each row is one set of parameters
-# #   Output: data frame of parameters & statistics, each row contains statistics for one set of parameters:
-# #           first columns = input parameters
-# #           next columns = summary statistics
-# model <- function(parameters, parallel = TRUE) {
-#     nNoise <- 0
-#     if (exists("nSimulations")) nSimulations <<- nSimulations + nrow(parameters)
-#     #   Make simulations & compute summary statistics (allele count)
-#     if (parallel) {
-#         library(parallel)
-#         library(pbapply)
-#         library(data.table)
-#         cl <- makePSOCKcluster(detectCores() - 1)
-#         clusterExport(cl, varlist = c("SFS_model"))
-#         stats <- pblapply(
-#             cl = cl, X = 1:nrow(parameters),
-#             FUN = function(i) {
-#                 mut_rate <<- parameters$theta[i]
-#                 # mut_rate <- 1e-6
-#                 SFS_model(mut_rate = mut_rate, generation_num = 100)
-#             }
-#         )
-#         stopCluster(cl)
-#         stats <- rbindlist(stats)
-#         class(stats) <- "data.frame"
-#     } else {
-#         stats <- c()
-#         for (i in 1:nrow(parameters)) {
-#             mut_rate <<- parameters$theta[i]
-#             stats <- rbind(stats, SFS_model(mut_rate = mut_rate, generation_num = 100))
-#         }
-#     }
-#     #   Add noise statistics
-#     noise <- matrix(runif(nrow(parameters) * nNoise), nrow(parameters), nNoise)
-#     #   Add column names
-#     data <- data.frame(cbind(stats, noise))
-#     if (nNoise > 0) {
-#         colnames(data) <- c(
-#             colnames(stats),
-#             paste0("noise_", c(1:nNoise))
-#         )
-#     }
-#     return(data)
-# }
+# ===========================Model for the Site Frequency Spectrum (SFS)
+#   Input:  data frame of parameters, each row is one set of parameters
+#   Output: data frame of parameters & statistics, each row contains statistics for one set of parameters:
+#           first columns = input parameters
+#           next columns = summary statistics
+model <- function(parameters, parallel = TRUE) {
+    nNoise <- 0
+    if (exists("nSimulations")) nSimulations <<- nSimulations + nrow(parameters)
+    #   Make simulations & compute summary statistics (allele count)
+    if (parallel) {
+        library(parallel)
+        library(pbapply)
+        library(data.table)
+        cl <- makePSOCKcluster(detectCores() - 1)
+        clusterExport(cl, varlist = c("SFS_model"))
+        stats <- pblapply(
+            cl = cl, X = 1:nrow(parameters),
+            FUN = function(i) {
+                mut_rate <<- parameters$theta[i]
+                # mut_rate <- 1e-6
+                SFS_model(mut_rate = mut_rate, generation_num = 100)
+            }
+        )
+        stopCluster(cl)
+        stats <- rbindlist(stats)
+        class(stats) <- "data.frame"
+    } else {
+        stats <- c()
+        for (i in 1:nrow(parameters)) {
+            mut_rate <<- parameters$theta[i]
+            stats <- rbind(stats, SFS_model(mut_rate = mut_rate, generation_num = 100))
+        }
+    }
+    #   Add noise statistics
+    noise <- matrix(runif(nrow(parameters) * nNoise), nrow(parameters), nNoise)
+    #   Add column names
+    data <- data.frame(cbind(stats, noise))
+    if (nNoise > 0) {
+        colnames(data) <- c(
+            colnames(stats),
+            paste0("noise_", c(1:nNoise))
+        )
+    }
+    return(data)
+}
 
-# SFS_model <- function(mut_rate = c(1e-5), generation_num = 100) {
-#     library(slimr)
-#     library(adegenet)
-#     generation_num <- 100
-#     mut_rate <- mut_rate
-#     script_1 <- slim_script(
-#         slim_block(initialize(), {
-#             mut_rate <- r_inline(mut_rate)
-#             initializeMutationRate(mut_rate)
-#             ## m1 mutation type: neutral
-#             initializeMutationType("m1", 0.5, "e", 0.0)
-#             ## Mean selection coefficient for Exponential Distribution
-#             ## M1 is the neutral mutation type
+SFS_model <- function(mut_rate = c(1e-5), generation_num = 100) {
+    library(slimr)
+    library(adegenet)
+    generation_num <- 100
+    mut_rate <- mut_rate
+    script_1 <- slim_script(
+        slim_block(initialize(), {
+            mut_rate <- r_inline(mut_rate)
+            initializeMutationRate(mut_rate)
+            ## m1 mutation type: neutral
+            initializeMutationType("m1", 0.5, "e", 0.0)
+            ## Mean selection coefficient for Exponential Distribution
+            ## M1 is the neutral mutation type
 
-#             ## g1 genomic element type: uses m1 for all mutations
-#             initializeGenomicElementType("g1", m1, 1.0)
+            ## g1 genomic element type: uses m1 for all mutations
+            initializeGenomicElementType("g1", m1, 1.0)
 
-#             ## uniform chromosome of length 100 kb
-#             initializeGenomicElement(g1, 0, 99999)
-#             ## uniform recombination along the chromosome
-#             initializeRecombinationRate(0)
-#         }),
-#         slim_block(1, early(), {
-#             ## create a population of 33 individuals
-#             sim.addSubpop("p1", 100)
-#         }),
-#         slim_block(1, 100, late(), {
-#             # create an output
-#             r_output_full("out", do_every = 10)
-#         }),
-#         slim_block(100, late(), {
-#             sim.simulationFinished()
-#         })
-#     )
-#     # ===================================================RUN SLIM SCRIPT
-#     slim_result <- slim_run(script_1, show_output = TRUE, capture_output = TRUE)
-#     mutlist <- slim_extract_full(slim_result$output_data, type = "genomes")[slim_extract_full(slim_result$output_data, type = "genomes")["generation"] == generation_num, ]["mut_list"]
-#     mutlist_all <- unlist(mutlist)
-#     mutlist_all[mutlist_all == 0] <- max(mutlist_all) + 1
-#     sfs <- tabulate(tabulate(mutlist_all))
-#     mutation_count <- sum(sfs)
-#     # ====================================================GET STATISTICS
-#     stats <- data.frame(matrix(c(mut_rate, mutation_count, sfs[1:20]), nrow = 1))
-#     colnames(stats) <- c("theta", "Mutation_count_S", paste0("SFS_", 1:20))
-#     return(stats)
-# }
-# # =====================================================Target statistics
-# theta <- 2e-4
-# parameters_ground_truth <- data.frame(
-#     theta = theta
-# )
-# set.seed(1)
-# statistics_target <- model(parameters = parameters_ground_truth, parallel = FALSE)[-c(1:ncol(parameters_ground_truth))]
-# print(statistics_target)
-# # ======================================Model for parameter perturbation
-# #   Input:  data frame of parameters, each row is one set of parameters
-# #   Output: data frame of parameters, after perturbation
-# perturb <- function(parameters) {
-#     for (i in 1:ncol(parameters)) parameters[[i]] <- parameters[[i]] + runif(nrow(parameters), min = -1, max = 1)
-#     return(parameters)
-# }
-# # ======================================Define ranges for the parameters
-# range <- data.frame(
-#     parameter = c("theta"),
-#     min = c(1e-5),
-#     max = c(1e-3)
-# )
-# # ========================================Initial guesses for parameters
-# # ====================================(sampled from prior distributions)
-# theta <- runif(100000, 1e-5, 1e-)
-# parameters_initial <- data.frame(
-#     theta = theta
-# )
-# # ====================================Labels for parameters in the plots
-# parameters_labels <- data.frame(
-#     parameter = c("theta"),
-#     label = c(deparse(expression(theta)))
-# )
-# # ================================================================ABC-RF
-# #---Run ABC-RF
-# abcrf_results <- smcrf(
-#     method = "smcrf-single-param",
-#     statistics_target = statistics_target,
-#     parameters_initial = parameters_initial,
-#     model = model,
-#     perturb = perturb,
-#     range = range,
-#     nParticles = rep(500, 1),
-#     parallel = TRUE
-# )
-# #---Plot posterior marginal distributions against other methods
-# plots <- plot_compare_marginal(
-#     abc_results = abcrf_results,
-#     parameters_truth = parameters_ground_truth,
-#     parameters_labels = parameters_labels,
-#     plot_statistics = TRUE
-# )
+            ## uniform chromosome of length 100 kb
+            initializeGenomicElement(g1, 0, 99999)
+            ## uniform recombination along the chromosome
+            initializeRecombinationRate(0)
+        }),
+        slim_block(1, early(), {
+            ## create a population of 33 individuals
+            sim.addSubpop("p1", 100)
+        }),
+        slim_block(1, 100, late(), {
+            # create an output
+            r_output_full("out", do_every = 10)
+        }),
+        slim_block(100, late(), {
+            sim.simulationFinished()
+        })
+    )
+    # ===================================================RUN SLIM SCRIPT
+    slim_result <- slim_run(script_1, show_output = TRUE, capture_output = TRUE)
+    mutlist <- slim_extract_full(slim_result$output_data, type = "genomes")[slim_extract_full(slim_result$output_data, type = "genomes")["generation"] == generation_num, ]["mut_list"]
+    mutlist_all <- unlist(mutlist)
+    mutlist_all[mutlist_all == 0] <- max(mutlist_all) + 1
+    sfs <- tabulate(tabulate(mutlist_all))
+    mutation_count <- sum(sfs)
+    # ====================================================GET STATISTICS
+    # stats <- data.frame(matrix(c(mut_rate, mutation_count, sfs[1:20]), nrow = 1))
+    stats <- data.frame(matrix(c(mut_rate, mutation_count), nrow = 1))
+    # colnames(stats) <- c("theta", "Mutation_count_S", paste0("SFS_", 1:20))
+    colnames(stats) <- c("theta", "Mutation_count_S")
+    return(stats)
+}
+# =====================================================Target statistics
+theta <- 2e-4
+parameters_ground_truth <- data.frame(
+    theta = theta
+)
+set.seed(1)
+statistics_target <- model(parameters = parameters_ground_truth, parallel = FALSE)[-c(1:ncol(parameters_ground_truth))]
+print(statistics_target)
+# ======================================Model for parameter perturbation
+#   Input:  data frame of parameters, each row is one set of parameters
+#   Output: data frame of parameters, after perturbation
+perturb <- function(parameters) {
+    for (i in 1:ncol(parameters)) parameters[[i]] <- parameters[[i]] + runif(nrow(parameters), min = -1, max = 1)
+    return(parameters)
+}
+# ======================================Define ranges for the parameters
+range <- data.frame(
+    parameter = c("theta"),
+    min = c(1e-5),
+    max = c(1e-3)
+)
+# ========================================Initial guesses for parameters
+# ====================================(sampled from prior distributions)
+theta <- runif(100000, 1e-5, 1e-3)
+parameters_initial <- data.frame(
+    theta = theta
+)
+# ====================================Labels for parameters in the plots
+parameters_labels <- data.frame(
+    parameter = c("theta"),
+    label = c(deparse(expression(theta)))
+)
+# ================================================================ABC-RF
+#---Run ABC-RF
+abcrf_results <- smcrf(
+    method = "smcrf-single-param",
+    statistics_target = statistics_target,
+    parameters_initial = parameters_initial,
+    model = model,
+    perturb = perturb,
+    range = range,
+    nParticles = rep(50, 1),
+    parallel = TRUE
+)
+#---Plot posterior marginal distributions against other methods
+plots <- plot_compare_marginal(
+    abc_results = abcrf_results,
+    parameters_truth = parameters_ground_truth,
+    parameters_labels = parameters_labels,
+    plot_statistics = TRUE
+)
 
 # # save(abcrf_results,file="ABCRF_results.rda")
 
-# # ========================================
-# #   Plot the out-of-bag estimates (equivalent to cross-validation)
-# abcrf_results[["Iteration_1"]][["rf_model"]][["model.rf"]]$predictions
-# abcrf_results$Iteration_1$rf_model$model.rf$predictions
-# abcrf_results$Iteration_1$parameters$theta
-# png(paste0("NEUTRAL_abcrf_theta_out_of_bag.png"))
-# plot(abcrf_results$Iteration_1$parameters$theta,
-#     abcrf_results$Iteration_1$rf_model$model.rf$predictions,
-#     xlab = "True value",
-#     ylab = "Out-of-bag estimate"
-# ) + abline(a = 0, b = 1, col = "red")
-# dev.off()
-# #   Can the error be lowered by increasing the number of trees?
-# library(abcrf)
-# oob_error <- err.regAbcrf(abcrf_results$Iteration_1$rf_model, training = abcrf_results$Iteration_1$reference, paral = T)
-# png(paste0("NEUTRAL_abcrf_theta_error_by_ntree.png"))
-# plot(oob_error[, "ntree"], oob_error[, "oob_mse"], type = "l", xlab = "Number of trees", ylab = "Out-of-bag MSE")
-# dev.off()
-# #   Variance Importance of each statistic in inferring gamma
-# png(paste0("NEUTRAL_abcrf_theta_variance_importance.png"), width = 1500, height = 800, res = 150)
-# n.var <- min(22, length(abcrf_results$Iteration_1$rf_model$model.rf$variable.importance))
-# imp <- abcrf_results$Iteration_1$rf_model$model.rf$variable.importance
-# names(imp) <- colnames(statistics_target)
-
 # ========================================
-parameters <- data.frame(parameters_initial[1:500, ])
-colnames(parameters) <- colnames(parameters_initial)
-reference <- model(parameters = parameters)
-model_rf <- regAbcrf(
-                formula = as.formula(paste0("theta", " ~ .")),
-                data = mini_reference,
-                paral = parallel
-            )
-posterior_gamma_RF <- predict(
-                object = RFmodel,
-                obs = statistics_target,
-                training = mini_reference,
-                paral = parallel,
-                rf.weights = T
-            )
+#   Plot the out-of-bag estimates (equivalent to cross-validation)
+abcrf_results[["Iteration_1"]][["rf_model"]][["model.rf"]]$predictions
+abcrf_results$Iteration_1$rf_model$model.rf$predictions
+abcrf_results$Iteration_1$parameters$theta
+png(paste0("NEUTRAL_abcrf_theta_out_of_bag.png"))
+plot(abcrf_results$Iteration_1$parameters$theta,
+    abcrf_results$Iteration_1$rf_model$model.rf$predictions,
+    xlab = "True value",
+    ylab = "Out-of-bag estimate"
+) + abline(a = 0, b = 1, col = "red")
+dev.off()
+#   Can the error be lowered by increasing the number of trees?
+library(abcrf)
+oob_error <- err.regAbcrf(abcrf_results$Iteration_1$rf_model, training = abcrf_results$Iteration_1$reference, paral = T)
+png(paste0("NEUTRAL_abcrf_theta_error_by_ntree.png"))
+plot(oob_error[, "ntree"], oob_error[, "oob_mse"], type = "l", xlab = "Number of trees", ylab = "Out-of-bag MSE")
+dev.off()
 #   Variance Importance of each statistic in inferring gamma
-png(paste0("NEUTRAL_abcrf_test_variance_importance.png"), width = 1500, height = 800, res = 150)
-n.var <- min(22, length(model_rf$model.rf$variable.importance))
-imp <- model_rf$model.rf$variable.importance
+png(paste0("NEUTRAL_abcrf_theta_variance_importance.png"), width = 1500, height = 800, res = 150)
+n.var <- min(22, length(abcrf_results$Iteration_1$rf_model$model.rf$variable.importance))
+imp <- abcrf_results$Iteration_1$rf_model$model.rf$variable.importance
 names(imp) <- colnames(statistics_target)
 ord <- rev(order(imp, decreasing = TRUE)[1:n.var])
+# colors <- group_colors[groups[ord]]
 xmin <- 0
 xlim <- c(xmin, max(imp) + 1)
 dotchart(imp[ord], pch = 19, xlab = "Variable Importance", ylab = "", xlim = xlim, main = NULL, bg = "white", cex = 0.7)
+# legend("bottomright", legend = names(group_colors), fill = group_colors, bty = "n", cex = 0.7)
 dev.off()
-
+# ========================================
+# parameters <- data.frame(parameters_initial[1:100, ])
+# colnames(parameters) <- colnames(parameters_initial)
+# reference <- model(parameters = parameters)
+# library(abcrf)
+# model_rf <- regAbcrf(
+#     formula = as.formula(paste0("theta", " ~ .")),
+#     data = reference,
+#     paral = TRUE
+# )
+# posterior_gamma_RF <- predict(
+#     object = model_rf,
+#     obs = statistics_target,
+#     training = reference,
+#     paral = TRUE,
+#     rf.weights = T
+# )
+#   Variance Importance of each statistic in inferring gamma
+# png(paste0("NEUTRAL_abcrf_test_variance_importance.png"), width = 1500, height = 800, res = 150)
+# n.var <- min(30, length(model_rf$model.rf$variable.importance))
+# imp <- model_rf$model.rf$variable.importance
+# names(imp) <- colnames(statistics_target)
+# ord <- rev(order(imp, decreasing = TRUE)[1:n.var])
+# xmin <- 0
+# xlim <- c(xmin, max(imp) + 1)
+# dotchart(imp[ord], pch = 19, xlab = "Variable Importance", ylab = "", xlim = xlim, main = NULL, bg = "white", cex = 0.7)
+# dev.off()
+# imp
 # groups <- c()
 # for (i in 1:length(names(imp))) {
 #     if (any(grepl(names(imp)[i], copy_number_stats, fixed = TRUE))) {
@@ -232,14 +241,14 @@ dev.off()
 
 # group_colors <- c("copy_number_stats" = "#00BA38", "phylo_balance_stats" = "#F8766D", "phylo_tip_stats" = "#619CFF")
 
-imp
-ord <- rev(order(imp, decreasing = TRUE)[1:n.var])
-# colors <- group_colors[groups[ord]]
-xmin <- 0
-xlim <- c(xmin, max(imp) + 1)
-dotchart(imp[ord], pch = 19, xlab = "Variable Importance", ylab = "", xlim = xlim, main = NULL, bg = "white", cex = 0.7)
-# legend("bottomright", legend = names(group_colors), fill = group_colors, bty = "n", cex = 0.7)
-dev.off()
+# imp
+# ord <- rev(order(imp, decreasing = TRUE)[1:n.var])
+# # colors <- group_colors[groups[ord]]
+# xmin <- 0
+# xlim <- c(xmin, max(imp) + 1)
+# dotchart(imp[ord], pch = 19, xlab = "Variable Importance", ylab = "", xlim = xlim, main = NULL, bg = "white", cex = 0.7)
+# # legend("bottomright", legend = names(group_colors), fill = group_colors, bty = "n", cex = 0.7)
+# dev.off()
 # # =====================================================Target statistics
 # mut_rates <- runif(100, 1e-6, 2e-5)
 # parameters_initial <- data.frame(
