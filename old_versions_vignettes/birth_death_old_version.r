2a40358fe1b8cafb64b8102877d0543340846bc8
ZijinXiang <zx2406@columbia.edu>
2024-05-28 16:54:23 -0400

    cleaning directory

---
+++ old_versions_vignettes/birth_death_old_version.r
@@ -0,0 +1,246 @@
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Khanh - Macbook
+# R_workplace <- "/Users/dinhngockhanh/Library/CloudStorage/GoogleDrive-knd2127@columbia.edu/My Drive/RESEARCH AND EVERYTHING/Projects/GITHUB/SMC-RF/vignettes"
+# R_libPaths <- ""
+# R_libPaths_extra <- "/Users/dinhngockhanh/Library/CloudStorage/GoogleDrive-knd2127@columbia.edu/My Drive/RESEARCH AND EVERYTHING/Projects/GITHUB/SMC-RF/R"
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Zijin - Macbook
+# R_workplace <- "/Users/xiangzijin/Documents/ABC_SMCRF/0224_test/hierarchical"
+# R_libPaths <- ""
+# R_libPaths_extra <- "/Users/xiangzijin/SMC-RF/R"
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Zhihan - Macbook
+R_workplace <- "/Users/lexie/Documents/DNA/SMC-RF/vignettes/birth_death/test"
+R_libPaths <- ""
+R_libPaths_extra <- "/Users/lexie/Documents/DNA/SMC-RF/R"
+# =======================================SET UP FOLDER PATHS & LIBRARIES
+.libPaths(R_libPaths)
+library(ggplot2)
+library(gridExtra)
+library(grid)
+library(invgamma)
+setwd(R_libPaths_extra)
+files_sources <- list.files(pattern = "\\.[rR]$")
+sapply(files_sources, source)
+setwd(R_workplace)
+
+
+
+# set.seed(1)
+
+
+
+# =====================================Model for the birth-death process
+#   Input:  data frame of parameters, each row is one set of parameters
+#   Output: data frame of parameters & statistics, each row contains statistics for one set of parameters:
+#           first columns = input parameters
+#           next columns = summary statistics
+model <- function(parameters, parallel = TRUE) {
+    nTimes <- 25
+    nNoise <- 0
+    if (exists("nSimulations")) nSimulations <<- nSimulations + nrow(parameters)
+    #   Make simulations & compute summary statistics (population sizes at each time point)
+    if (parallel) {
+        library(parallel)
+        library(pbapply)
+        library(data.table)
+        cl <- makePSOCKcluster(detectCores() - 1)
+        clusterExport(cl, varlist = c("BD_model"))
+        stats <- pblapply(
+            cl = cl, X = 1:nrow(parameters),
+            FUN = function(i) BD_model(parameters$lambda[i], parameters$mu[i], nTimes)
+        )
+        stopCluster(cl)
+        stats <- rbindlist(stats)
+        class(stats) <- "data.frame"
+    } else {
+        stats <- c()
+        for (i in 1:nrow(parameters)) {
+            stats <- rbind(stats, BD_model(parameters$lambda[i], parameters$mu[i], nTimes))
+        }
+    }
+    #   Add noise statistics
+    noise <- matrix(runif(nrow(parameters) * nNoise), nrow(parameters), nNoise)
+    data <- cbind(stats, noise)
+    #   Add column names
+    data <- data.frame(data)
+    if (nNoise > 0) {
+        colnames(data) <- c(
+            colnames(stats),
+            paste0("noise_", c(1:nNoise))
+        )
+    }
+    return(data)
+}
+BD_model <- function(lambda, mu, nTimes) {
+    #   This simulates a birth-death process
+    #   Input:  lambda = birth rate
+    #           mu = death rate
+    times <- seq(1, nTimes) / nTimes
+    npt <- length(times)
+    alpha <- rep(0, npt)
+    beta <- rep(0, npt)
+    tdiff <- c(times[1], diff(times))
+    Fsim <- rep(0, npt)
+    Zsim <- rep(0, npt)
+    # compute alpha and beta vectors
+    if (lambda == mu) {
+        alpha <- lambda * tdiff / (1 + lambda * tdiff)
+        beta <- alpha
+    } else {
+        elm <- exp((lambda - mu) * tdiff)
+        alpha <- mu * (elm - 1) / (lambda * elm - mu)
+        beta <- lambda * alpha / mu
+    }
+    # Now generate observations on F and Z. We assume F > 0 at each stage,
+    # so we are conditioning on Z > 0 at the end
+    zv <- 10 # start from single individual
+    Fsim[1] <- sample(1:zv, size = 1, prob = dbinom(1:zv, size = zv, 1 - alpha[1]))
+    Zsim[1] <- rnbinom(1, size = Fsim[1], prob = 1 - beta[1]) + Fsim[1]
+    for (j in seq(2, npt)) {
+        zv <- Zsim[j - 1]
+        Fsim[j] <- sample(1:zv, size = 1, prob = dbinom(1:zv, size = zv, 1 - alpha[j]))
+        Zsim[j] <- rnbinom(1, size = Fsim[j], prob = 1 - beta[j]) + Fsim[j]
+    }
+    stats <- data.frame(matrix(c(lambda, mu, Zsim[1:npt]), nrow = 1))
+    colnames(stats) <- c("lambda", "mu", paste0("Z_", 1:npt))
+    # stats <- data.frame(matrix(c(lambda, mu, Fsim[1:npt], Zsim[1:npt]), nrow = 1))
+    # colnames(stats) <- c("lambda", "mu", paste0("F_", 1:npt), paste0("Z_", 1:npt))
+    return(stats)
+}
+# =====================================================Target statistics
+parameters_truth <- data.frame(
+    lambda = 6,
+    mu = 3
+)
+# statistics_target <- model(parameters = parameters_truth, parallel = FALSE)[-c(1:ncol(parameters_truth))]
+# c(8,11,11,15,14,17,19,20,21,27,30,35,42,54,67,72,93,111,119,146,157,163,173,202,230)
+statistics_target <- data.frame(matrix(c(11, 13, 16, 17, 17, 21, 23, 23, 28, 32, 36, 48, 61, 68, 80, 96, 123, 135, 161, 173, 87, 197, 221, 253, 289), nrow = 1))
+colnames(statistics_target) <- c(paste0("Z_", 1:25))
+# ======================================Model for parameter perturbation
+#   Input:  data frame of parameters, each row is one set of parameters
+#   Output: data frame of parameters, after perturbation
+perturb <- function(parameters) {
+    for (i in 1:ncol(parameters)) parameters[[i]] <- parameters[[i]] + runif(nrow(parameters), min = -0.5, max = 0.5)
+    return(parameters)
+}
+# ======================================Define ranges for the parameters
+range <- data.frame(
+    parameter = c("lambda", "mu"),
+    min = c(0, 0),
+    max = c(15, 15)
+)
+# ========================================Initial guesses for parameters
+# ====================================(sampled from prior distributions)
+lambda <- runif(10000, 0, 15)
+mu <- runif(10000, 0, lambda)
+parameters_initial <- data.frame(
+    lambda = lambda,
+    mu = mu
+)
+# ====================================Labels for parameters in the plots
+parameters_labels <- data.frame(
+    parameter = c("lambda", "mu"),
+    label = c(deparse(expression(lambda)), deparse(expression(mu)))
+)
+# ==============================================================ABC-Rejection
+#---Run ABC
+abc_rej_results <- abc_rejection(
+    statistics_target = statistics_target,
+    model = model,
+    parameters_labels = parameters_labels,
+    prior_distributions = list(c("unif", 0, 15), c("unif", 0, 15)),
+    # prior_test = "X1 > X2",
+    tolerance_quantile = 0.02,
+    nParticles = 500000, progress_bar = TRUE
+)
+#---Plot posterior joint distributions against other methods
+plots_joint <- plot_compare_joint(
+    abc_results = abc_rej_results,
+    parameters_labels = parameters_labels
+)
+#---Plot marginal distributions compare
+plots_marginal <- plot_compare_marginal(
+    abc_results = abc_rej_results,
+    parameters_labels = parameters_labels,
+    parameters_truth = parameters_truth,
+)
+# ==============================================================ABC-MCMC
+#---Run ABC-MCMC
+abc_mcmc_results <- abc_mcmc(
+    statistics_target = statistics_target,
+    model = model,
+    parameters_labels = parameters_labels,
+    prior_distributions = list(c("unif", 0, 15), c("unif", 0, 15)),
+    # prior_test = "X1 > X2",
+    nParticles = 50000, method = "Marjoram", progress_bar = TRUE
+)
+#---Plot posterior joint distributions against other methods
+plots_joint <- plot_compare_joint( 
+    plots = plots_joint,
+    abc_results = abc_mcmc_results,
+    parameters_labels = parameters_labels
+)
+#---Plot marginal distributions compare
+plots_marginal <- plot_compare_marginal(
+    plots = plots_marginal,
+    abc_results = abc_mcmc_results,
+    parameters_labels = parameters_labels,
+    parameters_truth = parameters_truth,
+)
+# ===================================================================DRF
+#---Run ABC-RF
+abcrf_results <- smcrf(
+    method = "smcrf-multi-param",
+    statistics_target = statistics_target,
+    parameters_initial = parameters_initial,
+    model = model,
+    perturb = perturb,
+    range = range,
+    nParticles = rep(10000, 1),
+    parallel = TRUE
+)
+#---Plot posterior joint distributions against other methods
+plots_joint <- plot_compare_joint(
+    plots = plots_joint,
+    abc_results = abcrf_results,
+    parameters_labels = parameters_labels
+)
+#---Plot marginal distributions compare
+plots_marginal <- plot_compare_marginal(
+    plots = plots_marginal,
+    abc_results = abcrf_results,
+    parameters_labels = parameters_labels,
+)
+# ========================================SMC-RF for multiple parameters
+#---Run SMC-RF for multiple parameters
+smcrf_results_multi_param <- smcrf(
+    method = "smcrf-multi-param",
+    statistics_target = statistics_target,
+    parameters_initial = parameters_initial,
+    model = model,
+    perturb = perturb,
+    range = range,
+    nParticles = rep(10000, 5),
+    parallel = TRUE
+)
+#---Plot joint distributions
+plot_smcrf_joint(
+    smcrf_results = smcrf_results_multi_param,
+    parameters_labels = parameters_labels
+)
+#---Plot posterior joint distributions against other methods
+plots_joint <- plot_compare_joint(
+    plots = plots_joint,
+    abc_results = smcrf_results_multi_param,
+    parameters_labels = parameters_labels
+)
+#---Plot marginal distributions compare
+plots_marginal <- plot_compare_marginal(
+    plots = plots_marginal,
+    abc_results = smcrf_results_multi_param,
+    parameters_labels = parameters_labels,
+    parameters_truth = parameters_truth,
+)
+plot_smcrf_marginal(
+    smcrf_results = smcrf_results_multi_param,
+    parameters_labels = parameters_labels,
+    parameters_truth = parameters_truth,
+)
---
+++ old_versions_vignettes/sfs.r
@@ -0,0 +1,372 @@
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Khanh - Macbook
+# R_workplace <- "/Users/dinhngockhanh/Library/CloudStorage/GoogleDrive-knd2127@columbia.edu/My Drive/RESEARCH AND EVERYTHING/Projects/GITHUB/SMC-RF/vignettes"
+# R_libPaths <- ""
+# R_libPaths_extra <- "/Users/dinhngockhanh/Library/CloudStorage/GoogleDrive-knd2127@columbia.edu/My Drive/RESEARCH AND EVERYTHING/Projects/GITHUB/SMC-RF/R"
+# # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Zijin - Macbook
+R_workplace <- "/Users/xiangzijin/Documents/ABC_SMCRF/0329_sfs_for_paper/sfs_npop=1000_nsim=10000"
+R_libPaths <- ""
+R_libPaths_extra <- "/Users/xiangzijin/SMC-RF/R"
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Zhihan - Macbook
+# R_workplace <- "/Users/lexie/Documents/DNA/SMC-RF/vignettes"
+# R_libPaths <- ""
+# R_libPaths_extra <- "/Users/lexie/Documents/DNA/SMC-RF/R"
+# =======================================SET UP FOLDER PATHS & LIBRARIES
+.libPaths(R_libPaths)
+library(ggplot2)
+library(gridExtra)
+library(grid)
+library(invgamma)
+setwd(R_libPaths_extra)
+files_sources <- list.files(pattern = "\\.[rR]$")
+sapply(files_sources, source)
+setwd(R_workplace)
+
+
+
+# =========================Model for the Allele Frequency Spectrum (AFS)
+#   Input:  data frame of parameters, each row is one set of parameters
+#   Output: data frame of parameters & statistics, each row contains statistics for one set of parameters:
+#           first columns = input parameters
+#           next columns = summary statistics
+model <- function(parameters, parallel = TRUE) {
+    nSamples <- 1000
+    nNoise <- 0
+    if (exists("nSimulations")) nSimulations <<- nSimulations + nrow(parameters)
+    #   Make simulations & compute summary statistics (allele count)
+    if (parallel) {
+        library(parallel)
+        library(pbapply)
+        library(data.table)
+        cl <- makePSOCKcluster(detectCores() - 1)
+        clusterExport(cl, varlist = c("SFS_model"))
+        stats <- pblapply(
+            cl = cl, X = 1:nrow(parameters),
+            FUN = function(i) {
+                SFS_model(theta = parameters$theta[i], beta = 0, model_type = 1, n = nSamples)
+            }
+        )
+        stopCluster(cl)
+        stats <- rbindlist(stats)
+        class(stats) <- "data.frame"
+    } else {
+        stats <- c()
+        for (i in 1:nrow(parameters)) {
+            stats <- rbind(stats, SFS_model(theta = parameters$theta[i], beta = 0, model_type = 1, n = nSamples))
+        }
+    }
+    #   Add noise statistics
+    noise <- matrix(runif(nrow(parameters) * nNoise), nrow(parameters), nNoise)
+    #   Add column names
+    data <- data.frame(cbind(stats, noise))
+    if (nNoise > 0) {
+        colnames(data) <- c(
+            colnames(stats),
+            paste0("noise_", c(1:nNoise))
+        )
+    }
+    return(data)
+}
+SFS_model <- function(theta, beta, model_type, n) {
+    #   This simulates haplotype and site information
+    #   Model_type = 1 for regular coalescent (beta = 0)
+    #              = 2 for exponential growth coalescent model
+    #              = 3 for pure death process with rate 1
+    #---Set up arrays and initialisation
+    hist <- list() # list of sets of common ancestor labels
+    muts <- list() # list of mutation lists
+    muts[[2 * n - 1]] <- 0
+    mutindx <- 0 # label of last mutation to arise
+    #---Generate coalescence times and related vectors
+    ctimes <- rep(0, n) # coalescence times set to 0
+    stimes <- rep(0, n) # jump times set to 0
+    if (model_type != 3) {
+        #   Generate ordinary coalescence times
+        for (j in n:2) {
+            ctimes[j] <- rexp(1, rate = j * (j - 1) / 2)
+        }
+        if (model_type == 2) {
+            #   Calculate times from GT eqn (2.7)
+            #   First, compute standard coalescent jump times
+            stimes[n] <- ctimes[n]
+            for (l in seq(n - 1, 2)) {
+                stimes[l] <- ctimes[l] + stimes[l + 1]
+            }
+            #   Now compute exp growth jump times from GT (2.7)
+            for (j in seq(2, n)) {
+                stimes[j] <- log(1 + beta * stimes[j]) / beta
+            }
+            #   Then compute exp growth coal times by differencing
+            ctimes[n] <- stimes[n]
+            for (l in seq(n - 1, 2)) {
+                ctimes[l] <- stimes[l] - stimes[l + 1]
+            }
+        }
+    } else {
+        for (j in n:2) {
+            ctimes[j] <- rexp(1, rate = j)
+        }
+    }
+    #---Simulate tree topology
+    hist[[1]] <- 1:n
+    for (j in n:2) { # number of things to choose from
+        current <- hist[[n + 1 - j]] # current ancestor labels
+        indx <- sample(1:j, 2, replace = F) # which two to merge
+        current[indx[1]] <- 2 * n + 1 - j
+        current[indx[2]] <- 2 * n + 1 - j
+        hist[[n + 2 - j]] <- unique(current)
+    }
+    #---Pour mutations down the tree
+    for (j in n:2) {
+        parent <- setdiff(hist[[j]], hist[[j - 1]]) # parent label
+        children <- setdiff(hist[[j - 1]], hist[[j]]) # children's labels
+        for (l in children) {
+            s <- rpois(1, theta * ctimes[n + 2 - j] / 2)
+            if (s == 0) {
+                muts[[l]] <- muts[[parent]]
+            }
+            if (s > 0) {
+                mutstoadd <- seq(mutindx + 1, mutindx + s)
+                mutindx <- mutindx + s # new last mutation label
+                muts[[l]] <- union(muts[[parent]], mutstoadd)
+            }
+        }
+        notmerged <- setdiff(hist[[j - 1]], children) # Set of labels w/ no merges at this level
+        for (l in notmerged) {
+            s <- rpois(1, theta * ctimes[n + 2 - j] / 2) # generate mutations on branch label l
+            if (s > 0) {
+                mutstoadd <- seq(mutindx + 1, mutindx + s)
+                mutindx <- mutindx + s
+                muts[[l]] <- union(muts[[l]], mutstoadd)
+            }
+        }
+    }
+    # #---Find the haplotype distribution
+    # alleles <- table(sapply(muts[1:n], paste, collapse = " "))
+    # dimnames(alleles) <- NULL
+    # #---Find allele frequency spectrum
+    # afs <- tabulate(alleles, nbins = n)
+    # nalleles <- sum(afs)
+    # ncnt <- sum(1:n * afs)
+    #---Find site frequency spectrum
+    b1 <- unlist(muts[1:n])
+    b2 <- tabulate(b1)
+    sfs <- tabulate(b2, nbins = n - 1)
+    sval <- sum(sfs)
+    mean_sfs <- mean(sfs)
+    # #---Combine table of statistics:
+    # #---number of alleles, the homozygoity statistic and the first sqrt(n) frequencies in the ESF
+    # ss <- 0
+    # for (i in 1:n) ss <- ss + afs[i] * (i / n)^2
+    lvec <- floor(sqrt(n))
+    if (model_type == 1) {
+        # stats <- data.frame(matrix(c(theta, nalleles), nrow = 1))
+        # colnames(stats) <- c("theta", "Allele_count_K")
+        stats <- data.frame(matrix(c(theta, sval, mean_sfs, sfs[1:lvec]), nrow = 1))
+        colnames(stats) <- c("theta", "Mutation_count_S", "mean_sfs", paste0("SFS_", 1:lvec))
+        # stats <- data.frame(matrix(c(theta, nalleles, sval, ss, afs[1:lvec]), nrow = 1))
+        # colnames(stats) <- c("theta", "Allele_count_K", "Mutation_count_S", "Homozygosity_statistic_F", paste0("AFS_", 1:lvec))
+    } else {
+        stats <- data.frame(matrix(c(theta, beta, nalleles), nrow = 1))
+        colnames(stats) <- c("theta", "beta", "Allele_count_K")
+    }
+    return(stats)
+}
+# =====================================================Target statistics
+set.seed(1)
+theta <- runif(1, 1, 20)
+parameters_ground_truth <- data.frame(
+    theta = theta
+)
+statistics_target <- model(parameters = parameters_ground_truth, parallel = FALSE)[-c(1:ncol(parameters_ground_truth))]
+# ======================================Model for parameter perturbation
+#   Input:  data frame of parameters, each row is one set of parameters
+#   Output: data frame of parameters, after perturbation
+perturb <- function(parameters) {
+    for (i in 1:ncol(parameters)) parameters[[i]] <- parameters[[i]] + runif(nrow(parameters), min = -1, max = 1)
+    return(parameters)
+}
+# ======================================Define ranges for the parameters
+range <- data.frame(
+    parameter = c("theta"),
+    min = c(1),
+    max = c(20)
+)
+# ========================================Initial guesses for parameters
+# ====================================(sampled from prior distributions)
+set.seed(1)
+theta <- runif(10000, 1, 20)
+parameters_initial <- data.frame(
+    theta = theta
+)
+# ====================================Labels for parameters in the plots
+parameters_labels <- data.frame(
+    parameter = c("theta"),
+    label = c(deparse(expression(theta)))
+)
+# ================================================================ABC-RF
+#---Run ABC-RF
+abcrf_results <- smcrf(
+    method = "smcrf-single-param",
+    statistics_target = statistics_target,
+    parameters_initial = parameters_initial,
+    model = model,
+    perturb = perturb,
+    range = range,
+    nParticles = rep(10000, 1),
+    parallel = TRUE
+)
+#---Plot posterior marginal distributions against other methods
+plots <- plot_compare_marginal(
+    # plots = plots,
+    parameters_truth = parameters_ground_truth,
+    abc_results = abcrf_results,
+    parameters_labels = parameters_labels,
+    plot_statistics = TRUE
+)
+# ========================================
+save(abcrf_results, file = "abcrf_results.rda")
+#   Plot the out-of-bag estimates (equivalent to cross-validation)
+abcrf_results[["Iteration_1"]][["rf_model"]][["model.rf"]]$predictions
+abcrf_results$Iteration_1$rf_model$model.rf$predictions
+abcrf_results$Iteration_1$parameters$theta
+png(paste0("NEUTRAL_abcrf_theta_out_of_bag.png"))
+plot(abcrf_results$Iteration_1$parameters$theta,
+    abcrf_results$Iteration_1$rf_model$model.rf$predictions,
+    xlab = "True value",
+    ylab = "Out-of-bag estimate"
+) + abline(a = 0, b = 1, col = "red")
+dev.off()
+#   Can the error be lowered by increasing the number of trees?
+library(abcrf)
+oob_error <- err.regAbcrf(abcrf_results$Iteration_1$rf_model, training = abcrf_results$Iteration_1$reference, paral = T)
+png(paste0("NEUTRAL_abcrf_theta_error_by_ntree.png"))
+plot(oob_error[, "ntree"], oob_error[, "oob_mse"], type = "l", xlab = "Number of trees", ylab = "Out-of-bag MSE")
+dev.off()
+#   Variance Importance of each statistic in inferring gamma
+png(paste0("NEUTRAL_abcrf_theta_variance_importance.png"), width = 1500, height = 800, res = 150)
+n.var <- min(30, length(abcrf_results$Iteration_1$rf_model$model.rf$variable.importance))
+imp <- abcrf_results$Iteration_1$rf_model$model.rf$variable.importance
+names(imp) <- colnames(statistics_target)
+ord <- rev(order(imp, decreasing = TRUE)[1:n.var])
+xmin <- 0
+xlim <- c(xmin, max(imp) + 1)
+dotchart(imp[ord], pch = 19, xlab = "Variable Importance", ylab = "", xlim = xlim, main = NULL, bg = "white", cex = 0.7)
+dev.off()
+# # =========================================================ABC-rejection
+# #---Run ABC-rejection
+# abc_rej_results <- abc_rejection(
+#     statistics_target = statistics_target,
+#     model = model,
+#     parameters_labels = parameters_labels,
+#     prior_distributions = list(c("unif", 1, 20)),
+#     nParticles = 10000, tolerance_quantile = 0.1, progress_bar = TRUE
+# )
+# #---Plot posterior marginal distributions against other methods
+# plots <- plot_compare_marginal(
+#     abc_results = abc_rej_results,
+#     parameters_labels = parameters_labels,
+#     parameters_truth = parameters_ground_truth,
+#     plot_statistics = TRUE
+# )
+# # ==============================================================ABC-MCMC
+# #---Run ABC-rejection
+# abc_mcmc_results <- abc_mcmc(
+#     statistics_target = statistics_target,
+#     model = model,
+#     parameters_labels = parameters_labels,
+#     prior_distributions = list(c("unif", 1, 20)),
+#     nParticles = 1000, method = "Marjoram_original", progress_bar = TRUE
+# )
+# #---Plot posterior marginal distributions against other methods
+# plots <- plot_compare_marginal(
+#     plots = plots,
+#     abc_results = abc_mcmc_results,
+#     parameters_labels = parameters_labels,
+#     plot_statistics = TRUE
+# )
+# # ===============================================================ABC-SMC
+# #---Find minimum tolerance compatible with noisiness in model
+# parameters_test <- do.call(rbind, replicate(1000, parameters_ground_truth, simplify = FALSE))
+# statistics_test <- model(parameters = parameters_test)[-c(1:ncol(parameters_test))]
+# distance_matrix <- as.matrix(dist(statistics_test, method = "euclidean"))^2
+# tolerance_min <- sum(distance_matrix) / (nrow(distance_matrix) * (ncol(distance_matrix) - 1))
+# #---Run ABC-SMC
+# abc_smc_results <- abc_smc(
+#     statistics_target = statistics_target,
+#     model = model,
+#     parameters_labels = parameters_labels,
+#     prior_distributions = list(c("unif", 1, 20)),
+#     nParticles = 1000, method = "Beaumont", progress_bar = TRUE,
+#     tolerance = c(2 * tolerance_min, 1.5 * tolerance_min, tolerance_min),
+#     dist_weights = rep(1, ncol(statistics_target))
+# )
+# #---Plot posterior marginal distributions against other methods
+# plots <- plot_compare_marginal(
+#     plots = plots,
+#     abc_results = abc_smc_results,
+#     parameters_labels = parameters_labels,
+#     plot_statistics = TRUE
+# )
+
+# # ==========================================SMC-RF for single parameters
+# #---Run SMC-RF for single parameters
+# smcrf_results <- smcrf(
+#     method = "smcrf-single-param",
+#     statistics_target = statistics_target,
+#     parameters_initial = parameters_initial,
+#     model = model,
+#     perturb = perturb,
+#     range = range,
+#     nParticles = rep(1000, 10),
+#     parallel = TRUE
+# )
+# #---Plot marginal distributions
+# plot_smcrf_marginal(
+#     smcrf_results = smcrf_results,
+#     parameters_truth = parameters_ground_truth,
+#     parameters_labels = parameters_labels,
+#     plot_statistics = TRUE
+# )
+# #---Plot posterior marginal distributions against other methods
+# plots <- plot_compare_marginal(
+#     plots = plots,
+#     abc_results = smcrf_results,
+#     parameters_labels = parameters_labels,
+#     plot_statistics = TRUE
+# )
+
+# setwd("/Users/xiangzijin/Documents/ABC_SMCRF/0329_sfs_for_paper")
+# load("/Users/xiangzijin/Documents/ABC_SMCRF/0329_sfs_for_paper/sfs_npop=1000_nsim=10000/abcrf_results.rda")
+# load("/Users/xiangzijin/Documents/ABC_SMCRF/0329_sfs_for_paper/coala_length1000_npop=1000_nsim=10000/abcrf_results.rda")
+# sfs_S <- abcrf_results[["Iteration_1"]][["statistics"]][["Mutation_count_S"]]
+# sfs_mean <- abcrf_results[["Iteration_1"]][["statistics"]][["mean_sfs"]]
+# coala_S <- abcrf_results[["Iteration_1"]][["statistics"]][["Mutation_count_S"]]
+# coala_mean <- abcrf_results[["Iteration_1"]][["statistics"]][["mean_sfs"]]
+# qqplot()
+
+
+# # sfs_S
+# png(paste0("qqplot_S.png"))
+# qqplot(coala_S, sfs_S)
+# abline(0, 1)
+# dev.off()
+
+# png(paste0("qqplot_mean.png"))
+# qqplot(coala_mean, sfs_mean)
+# abline(0, 1)
+# dev.off()
+
+# png(paste0("scatter_mean.png"))
+# plot(x = sfs_mean, y = coala_mean)
+# abline(0, 1)
+# dev.off()
+
+
+
+# png(paste0("coala_theta_mean.png"))
+# plot(x = parameters_initial$theta, y = coala_mean, xlab = "theta", ylab = "coala_sfs_mean")
+# dev.off()
+
+# png(paste0("sfs_theta_mean.png"))
+# plot(x = parameters_initial$theta, y = sfs_mean, xlab = "theta", ylab = "sfs_sfs_mean")
+# dev.off()
