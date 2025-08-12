feb1661ea51e2c6b7193fce122f40afac5a34777
Dinh Ngoc Khanh <knd2127@columbia.edu>
2025-08-12 16:26:06 -0400

    .

---
+++ vignettes/Birth-death model.r
@@ -0,0 +1,289 @@
+library(abcsmcrf)
+set.seed(1)
+# =====================================Model for the birth-death process
+#   Input:  data frame of parameters, each row is one set of parameters
+#   Output: data frame of parameters & statistics, each row contains statistics for one set of parameters:
+#           first columns = input parameters
+#           next columns = summary statistics
+model <- function(parameters, parallel = TRUE) {
+    nTimes <- 25
+    if (exists("nSimulations")) nSimulations <<- nSimulations + nrow(parameters)
+    #   Make simulations & compute summary statistics (population sizes at each time point)
+    if (parallel) {
+        library(parallel)
+        library(pbapply)
+        library(data.table)
+        cl <- makePSOCKcluster(detectCores() - 1)
+        clusterExport(cl, varlist = c("BD_model"))
+        stats <- pblapply(
+            cl = cl, X = 1:nrow(parameters),
+            FUN = function(i) BD_model(parameters$lambda_minus_mu[i], parameters$mu_over_lambda[i], nTimes)
+        )
+        stopCluster(cl)
+        stats <- rbindlist(stats)
+        class(stats) <- "data.frame"
+    } else {
+        stats <- c()
+        for (i in 1:nrow(parameters)) {
+            stats <- rbind(stats, BD_model(parameters$lambda_minus_mu[i], parameters$mu_over_lambda[i], nTimes))
+        }
+    }
+    data <- data.frame(stats)
+    return(data)
+}
+BD_model <- function(lambda_minus_mu, mu_over_lambda, nTimes) {
+    #   This simulates a birth-death process
+    #   Input:  lambda = birth rate
+    #           mu = death rate
+    mu <- (lambda_minus_mu * mu_over_lambda) / (1 - mu_over_lambda)
+    lambda <- mu + lambda_minus_mu
+    times <- seq(1, nTimes) / nTimes
+    npt <- length(times)
+    alpha <- rep(0, npt)
+    beta <- rep(0, npt)
+    tdiff <- c(times[1], diff(times))
+    Fsim <- rep(0, npt)
+    Zsim <- rep(0, npt)
+    elm <- exp((lambda - mu) * tdiff)
+    alpha <- mu * (elm - 1) / (lambda * elm - mu)
+    beta <- lambda * alpha / mu
+    #   Now generate observations on F and Z.
+    #   We condition on F > 0 at each stage, i.e. Z_25 > 0
+    zv <- 10
+    Fsim[1] <- rbinom(1, size = zv, prob = 1 - alpha[1])
+    if (Fsim[1] == 0) {
+        Zsim[1] <- 0
+    } else {
+        Zsim[1] <- rnbinom(1, size = Fsim[1], prob = 1 - beta[1]) + Fsim[1]
+    }
+    for (j in seq(2, npt)) {
+        Fsim[j] <- rbinom(1, size = Zsim[j - 1], prob = 1 - alpha[j])
+        if (Fsim[j] == 0) {
+            Zsim[j] <- 0
+        } else {
+            Zsim[j] <- rnbinom(1, size = Fsim[j], prob = 1 - beta[j]) + Fsim[j]
+        }
+    }
+    if (Zsim[npt] == 0) {
+        stats <- data.frame(matrix(rep(NA, (npt + 2)), nrow = 1, ncol = npt + 2))
+    } else {
+        stats <- data.frame(matrix(c(lambda_minus_mu, mu_over_lambda, Zsim[1:npt]), nrow = 1))
+    }
+    colnames(stats) <- c("lambda_minus_mu", "mu_over_lambda", paste0("Z_", 1:npt))
+    return(stats)
+}
+# =====================================================Target statistics
+statistics_target <- data.frame(matrix(c(
+    11, 13, 16, 17, 17, 21, 23, 23, 28, 32, 36, 48, 61, 68, 80, 96, 123, 135, 161, 173, 187, 197, 221, 253, 289
+), nrow = 1))
+colnames(statistics_target) <- c(paste0("Z_", 1:25))
+parameters_target <- data.frame(
+    lambda_minus_mu = 3,
+    mu_over_lambda = 0.5
+)
+# ====================================Labels for parameters in the plots
+parameters_labels <- data.frame(
+    parameter = c("lambda_minus_mu", "mu_over_lambda"),
+    label = c(
+        deparse(expression(lambda - mu)),
+        deparse(expression(mu / lambda))
+    )
+)
+# =====================================True joint density for parameters
+#---Define the negative log-likelihood function
+nLL <- function(param) {
+    #   Changes for reparametrization
+    lambda_minus_mu <- param[1]
+    mu_over_lambda <- param[2]
+    if (mu_over_lambda == 1) {
+        return(-log(0))
+    } else {
+        mu <- (lambda_minus_mu * mu_over_lambda) / (1 - mu_over_lambda)
+        lambda <- mu + lambda_minus_mu
+
+        zvals <- c(10, unlist(statistics_target))
+        zvals <- unname(zvals)
+        times <- seq(1, 25) / 25
+        npt <- length(times)
+        alpha <- rep(0, npt)
+        beta <- rep(0, npt)
+        tdiff <- c(times[1], diff(times))
+        #   Compute alpha and beta vectors
+        if (lambda == mu) {
+            alpha <- lambda * tdiff / (1 + lambda * tdiff)
+            beta <- alpha
+        } else {
+            elm <- exp((lambda - mu) * tdiff)
+            alpha <- mu * (elm - 1) / (lambda * elm - mu)
+            beta <- lambda * alpha / mu
+        }
+        totLL <- 0.0
+        #   Now calculate log-likelihood from the decomposition
+        for (j in seq(1, npt)) {
+            n <- zvals[j]
+            m <- zvals[j + 1]
+            alph <- alpha[j]
+            bet <- beta[j]
+            mn <- min(m, n)
+            tot <- 0
+            for (l in 1:mn) {
+                tot <- tot + dbinom(l, size = n, prob = 1 - alph) *
+                    dnbinom(m - l, size = l, prob = 1 - bet)
+            }
+            totLL <- totLL - log(tot)
+        }
+        return(-totLL)
+    }
+}
+#---Define the prior distribution
+shape <- 3
+rate <- 1
+shape1 <- 5
+shape2 <- 5
+prior <- function(param) {
+    lambda_minus_mu <- param[1]
+    mu_over_lambda <- param[2]
+    if ((lambda_minus_mu < 0) || (mu_over_lambda > 1)) {
+        return(log(0))
+    } else {
+        return(dgamma(lambda_minus_mu, shape = shape, rate = rate, log = T) + dbeta(mu_over_lambda, shape1 = shape1, shape2 = shape2))
+    }
+}
+#---Compute the posterior distribution
+posterior <- function(param) {
+    return(nLL(param) + prior(param))
+}
+lambdas_minus_mu <- seq(0.1, 20, 0.1)
+mus_over_lambda <- seq(0.1, 0.99, 0.001)
+vec_lambda <- c()
+vec_mu <- c()
+vec_nLL <- c()
+cnt <- 0
+for (i in 1:length(lambdas_minus_mu)) {
+    for (j in 1:length(mus_over_lambda)) {
+        cnt <- cnt + 1
+        lambda <- lambdas_minus_mu[i]
+        mu <- mus_over_lambda[j]
+        vec_lambda <- c(vec_lambda, lambda)
+        vec_mu <- c(vec_mu, mu)
+        vec_nLL <- c(vec_nLL, posterior(c(lambda, mu)))
+    }
+}
+max_likelihood <- max(vec_nLL)
+lln_weights <- exp(vec_nLL - max_likelihood)
+lln_weights <- lln_weights / sum(sort(lln_weights))
+df_nLL <- data.frame(lambda_minus_mu = vec_lambda, mu_over_lambda = vec_mu, likelihood = vec_nLL)
+sampled_parameters <- df_nLL[sample(nrow(df_nLL), 2000, prob = lln_weights, replace = T), ]
+parameters_truth <- data.frame(
+    lambda_minus_mu = sampled_parameters$lambda_minus_mu + runif(nrow(sampled_parameters), min = -0.05, max = 0.05),
+    mu_over_lambda = sampled_parameters$mu_over_lambda + runif(nrow(sampled_parameters), min = -0.05, max = 0.05)
+)
+true_joint <- c()
+true_joint$likelihood <- df_nLL
+true_joint$method <- "true-joint"
+true_joint$parameters_truth <- parameters_truth
+#---Plot posterior joint distributions against other methods
+plots_joint <- plot_compare_joint(
+    abc_results = true_joint,
+    parameters_labels = parameters_labels
+)
+# ====================================================Prior distribution
+dprior <- function(parameters, parameter_id = "all") {
+    probs <- rep(1, nrow(parameters))
+    if (parameter_id %in% c("all", "lambda_minus_mu")) {
+        probs <- probs * dgamma(parameters[["lambda_minus_mu"]], shape = shape, rate = rate)
+    }
+    if (parameter_id %in% c("all", "mu_over_lambda")) {
+        probs <- probs * dbeta(parameters[["mu_over_lambda"]], shape1 = shape1, shape2 = shape2)
+    }
+    return(probs)
+}
+rprior <- function(Nparameters) {
+    lambda_minus_mu <- rgamma(Nparameters, shape = shape, rate = rate)
+    mu_over_lambda <- rbeta(Nparameters, shape1 = shape1, shape2 = shape2)
+    return(data.frame(lambda_minus_mu = lambda_minus_mu, mu_over_lambda = mu_over_lambda))
+}
+# ============================================================ABC-SMC-RF
+#---Run ABC-RF
+abcrf_results <- smcrf(
+    method = "smcrf-single-param",
+    statistics_target = statistics_target,
+    model = model,
+    rprior = rprior,
+    dprior = dprior,
+    perturbation = "Uniform",
+    perturbation_parameters = data.frame(
+        lambda_minus_mu = rep(1, 4),
+        mu_over_lambda = rep(0.05, 4)
+    ),
+    nParticles = rep(5000, 4),
+    ntree = 2500,
+    save_model = FALSE,
+    parallel = TRUE,
+    model_redo_if_NA = TRUE
+)
+#---Plot qqplots against other methods
+plots_compare_qqplot <- plot_compare_qqplot(
+    abc_results = abcrf_results,
+    parameters_truth = parameters_truth,
+    parameters_labels = parameters_labels
+)
+#---Plot marginal distributions against other methods
+plots_marginal <- plot_compare_marginal(
+    abc_results = abcrf_results,
+    parameters_labels = parameters_labels,
+    plot_hist = TRUE,
+    position_x = 0.9,
+    position_y = 0.5
+)
+# ===========================================================ABC-SMC-DRF
+smcrf_results_multi_param <- smcrf(
+    method = "smcrf-multi-param",
+    statistics_target = statistics_target,
+    model = model,
+    rprior = rprior,
+    dprior = dprior,
+    perturbation = "Uniform",
+    perturbation_parameters = data.frame(
+        lambda_minus_mu = rep(1, 4),
+        mu_over_lambda = rep(0.05, 4)
+    ),
+    nParticles = rep(5000, 4),
+    save_model = FALSE,
+    num.trees = 2500,
+    splitting.rule = "FourierMMD",
+    model_redo_if_NA = TRUE,
+    parallel = TRUE
+)
+#---Plot posterior joint distributions against other methods
+plots_joint <- plot_compare_joint(
+    plots = plots_joint,
+    lims = data.frame(
+        parameter = c("lambda_minus_mu", "mu_over_lambda"),
+        min = c(1, 0),
+        max = c(5, 1)
+    ),
+    abc_results = smcrf_results_multi_param,
+    parameters_labels = parameters_labels
+)
+#---Plot marginal distributions against other methods
+plots_marginal <- plot_compare_marginal(
+    plots = plots_marginal,
+    abc_results = smcrf_results_multi_param,
+    parameters_labels = parameters_labels,
+    plot_hist = TRUE,
+    position_x = 0.9,
+    position_y = 0.1
+)
+#---Plot qqplots against other methods
+plots_compare_qqplot <- plot_compare_qqplot(
+    plots = plots_compare_qqplot,
+    abc_results = smcrf_results_multi_param,
+    lims = data.frame(
+        parameter = c("lambda_minus_mu", "mu_over_lambda"),
+        min = c(1, 0),
+        max = c(5, 1)
+    ),
+    parameters_truth = parameters_truth,
+    parameters_labels = parameters_labels
+)
---
+++ vignettes/Controlled birth-death model.r
@@ -0,0 +1,314 @@
+library(abcsmcrf)
+set.seed(1)
+# ===================================Function to compute estimation of m
+estimated_mean <- function(abc_results) {
+    nIterations <- abc_results[["nIterations"]]
+    ref_df <- abc_results[[paste0("Iteration_", nIterations + 1)]]$parameters_unperturbed
+    nb_rows <- nrow(ref_df)
+    m <- numeric(nb_rows)
+    for (i in seq_len(nb_rows)) {
+        kappa_i <- ref_df$kappa[i]
+        js <- 0:kappa_i
+        prob_js <- unlist(ref_df[i, paste0("prob_", js)])
+        m[i] <- sum(js * prob_js)
+    }
+    abc_results[[paste0("Iteration_", nIterations + 1)]]$parameters_unperturbed$m <- m
+    return(abc_results)
+}
+# =====================================Model for the birth-death process
+#   Input:  data frame of parameters, each row is one set of parameters
+#   Output: data frame of parameters & statistics, each row contains statistics for one set of parameters:
+#           first columns = input parameters
+#           next columns = summary statistics
+model <- function(parameters, parallel = TRUE) {
+    nTimes <- 10
+    K_max <- 7
+    if (exists("nSimulations")) nSimulations <<- nSimulations + nrow(parameters)
+    #   Make simulations & compute summary statistics (population sizes at each time point)
+    if (parallel) {
+        library(parallel)
+        library(pbapply)
+        library(data.table)
+        cl <- makePSOCKcluster(detectCores() - 1)
+        clusterExport(cl, varlist = c("BD_model"))
+        stats <- pblapply(
+            cl = cl, X = 1:nrow(parameters),
+            FUN = function(i) BD_model(parameters$kappa[i], as.numeric(parameters[i, c(paste0("prob_", 0:K_max))]), parameters$gamma[i], nTimes)
+        )
+        stopCluster(cl)
+        stats <- rbindlist(stats)
+        class(stats) <- "data.frame"
+    } else {
+        stats <- c()
+        for (i in 1:nrow(parameters)) {
+            stats <- rbind(stats, BD_model(parameters$kappa[i], as.numeric(parameters[i, c(paste0("prob_", 0:K_max))]), parameters$gamma[i], nTimes))
+        }
+    }
+    data <- stats
+    return(data)
+}
+BD_model <- function(kappa, prob_X, gamma, nTimes) {
+    #   This simulates a controlled branching process
+    #   Input:  lambda = birth rate
+    #           mu = death rate
+    kappa <- floor(kappa)
+    phi_sim <- rep(0, nTimes)
+    Zsim <- rep(0, nTimes + 1)
+    #   Now generate observations on phi and Z
+    zv <- 1
+    Zsim[1] <- zv
+    for (j in seq(2, length(Zsim))) {
+        if (Zsim[j - 1] == 0) {
+            size_sim <- 0
+        } else {
+            size_sim <- Zsim[j - 1] + floor(log(Zsim[j - 1]))
+        }
+        phi_sim[j - 1] <- rbinom(1, size = size_sim, prob = gamma)
+        if (phi_sim[j - 1] == 0) {
+            Zsim[j] <- 0
+        } else {
+            if (phi_sim[j - 1] < .Machine$integer.max) {
+                prob_X_new <- rep(0, length(prob_X))
+                prob_X_new[1:(kappa + 1)] <- prob_X[1:(kappa + 1)]
+                X <- sample(0:(length(prob_X) - 1), size = phi_sim[j - 1], replace = TRUE, prob = prob_X_new)
+                Zsim[j] <- sum(X)
+            } else {
+                Zsim[j] <- 0
+            }
+        }
+    }
+    if (Zsim[length(Zsim)] == 0) {
+        stats <- data.frame(matrix(rep(NA, (length(Zsim) + 3 + length(prob_X))), nrow = 1, ncol = (length(Zsim) + 3 + length(prob_X))))
+    } else {
+        stats <- data.frame(matrix(c(kappa, prob_X[seq_along(prob_X)], gamma, Zsim[1:length(Zsim)], phi_sim[length(Zsim) - 1]), nrow = 1))
+    }
+    colnames(stats) <- c("kappa", paste0("prob_", 0:(length(prob_X) - 1)), "gamma", paste0("Z_", 0:(length(Zsim) - 1)), paste0("Phi_", length(Zsim) - 2))
+    return(stats)
+}
+# =====================================================Target statistics
+statistics_target <- data.frame(matrix(c(
+    1, 4, 12, 30, 84, 249, 728, 2148, 6165, 17883, 51412, 14281
+), nrow = 1))
+colnames(statistics_target) <- c(paste0("Z_", 0:10), "Phi_9")
+K_max <- 7
+rho <- 0.9
+kappa <- 4
+gamma <- 0.8
+prob_X <- rep(0, K_max + 1)
+x <- 0:kappa
+prob_X[1:(kappa + 1)] <- dbinom(x, size = kappa, prob = rho)
+parameters_target <- data.frame(
+    m = 3.6,
+    gamma = 0.8,
+    kappa = 4
+)
+# ======================================Model for parameter perturbation
+perturbation <- function(nb_iterations) {
+    prob_names <- paste0("prob_", seq_along(prob_X) - 1)
+    prob_matrix <- matrix(rep(rep(0.05, length(prob_X)), nb_iterations), nrow = nb_iterations)
+    prob_df <- as.data.frame(prob_matrix)
+    colnames(prob_df) <- prob_names
+    perturbation_parameters <- data.frame(
+        kappa = rep(3, nb_iterations),
+        gamma = rep(0.05, nb_iterations)
+    )
+    perturbation_parameters <- cbind(perturbation_parameters, prob_df)
+    return(perturbation_parameters)
+}
+# ====================================================Prior distribution
+dprior <- function(parameters, parameter_id = "all") {
+    probs <- rep(1, nrow(parameters))
+    if (parameter_id %in% c("all", "gamma")) {
+        probs <- probs * dbeta(parameters[["gamma"]], shape1 = 1, shape2 = 1)
+    }
+    if (parameter_id %in% c("all", "kappa")) {
+        invalid_kappa <- (parameters[["kappa"]] <= 0) | (parameters[["kappa"]] > (K_max + 1))
+        probs[invalid_kappa] <- 0
+        valid_idx <- !invalid_kappa
+        probs[valid_idx] <- probs[valid_idx] * 1 / (K_max - 1)
+    }
+    if (parameter_id %in% c("all", paste0("prob_", 0:K_max))) {
+        for (i in 1:nrow(parameters)) {
+            kappa_i <- floor(parameters$kappa[i])
+            condition_kappa <- (kappa_i >= 0) && (kappa_i < K_max + 1)
+            if (condition_kappa) {
+                x <- as.numeric(parameters[i, paste0("prob_", 0:kappa_i)])
+                if (all(x >= 0)) {
+                    probs[i] <- probs[i] * ddirichlet(x, alpha = rep(1, kappa_i + 1))
+                } else {
+                    probs[i] <- 0
+                }
+            } else {
+                probs[i] <- 0
+            }
+        }
+    }
+    return(probs)
+}
+rprior <- function(Nparameters) {
+    gamma <- rbeta(Nparameters, shape1 = 1, shape2 = 1)
+    kappa <- sample(2:K_max, size = Nparameters, replace = TRUE)
+    probabilities <- matrix(0, nrow = Nparameters, ncol = K_max + 1)
+    for (i in 1:Nparameters) {
+        prior_sim <- rdirichlet(1, alpha = rep(1, kappa[i] + 1))
+        probabilities[i, 1:(kappa[i] + 1)] <- prior_sim
+    }
+    probabilities <- as.data.frame(probabilities)
+    colnames(probabilities) <- paste0("prob_", 0:K_max)
+    parameter_df <- data.frame(kappa = kappa, gamma = gamma)
+    return(cbind(parameter_df, probabilities))
+}
+# ====================================Labels for parameters in the plots
+parameters_labels <- data.frame(
+    parameter = c("m", "gamma", "kappa"),
+    label = c(deparse(expression(m)), deparse(expression(gamma)), deparse(expression(kappa)))
+)
+# ===================================================================DRF
+#---Run DRF
+drf_results <- smcrf(
+    method = "smcrf-multi-param",
+    statistics_target = statistics_target,
+    model = model,
+    rprior = rprior,
+    dprior = dprior,
+    nParticles = rep(20000, 1),
+    num.trees = 2500,
+    save_model = FALSE,
+    splitting.rule = "FourierMMD",
+    parallel = TRUE,
+    model_redo_if_NA = TRUE,
+    honesty = FALSE
+)
+#---Compute m
+drf_results <- estimated_mean(drf_results)
+#---Plot posterior joint distributions against other methods
+plots_joint <- plot_compare_joint(
+    abc_results = drf_results,
+    parameters_labels = parameters_labels
+)
+#---Plot qqplots against other methods
+plots_compare_qqplot <- plot_compare_qqplot(
+    abc_results = drf_results,
+    parameters_truth = parameters_truth,
+    parameters_labels = parameters_labels,
+)
+#---Plot marginal distributions against other methods
+plots_marginal <- plot_compare_marginal(
+    abc_results = drf_results,
+    parameters_labels = parameters_labels,
+    parameters_truth = parameters_target,
+    plot_hist = TRUE,
+    position_x = 0.9,
+    position_y = 0.7
+)
+# ====================================================================RF
+#---Run ABC-RF
+abcrf_results <- smcrf(
+    method = "smcrf-single-param",
+    statistics_target = statistics_target,
+    model = model,
+    rprior = rprior,
+    dprior = dprior,
+    ntree = 2500,
+    nParticles = rep(20000, 1),
+    save_model = FALSE,
+    parallel = TRUE,
+    model_redo_if_NA = TRUE
+)
+#---Compute m
+abcrf_results <- estimated_mean(abcrf_results)
+#---Plot qqplots against other methods
+plots_compare_qqplot <- plot_compare_qqplot(
+    plots = plots_compare_qqplot,
+    abc_results = abcrf_results,
+    parameters_truth = parameters_truth,
+    parameters_labels = parameters_labels
+)
+#---Plot marginal distributions against other methods
+plots_marginal <- plot_compare_marginal(
+    plots = plots_marginal,
+    abc_results = abcrf_results,
+    parameters_labels = parameters_labels,
+    plot_hist = TRUE,
+    position_x = 0.9,
+    position_y = 0.5
+)
+# ================================================================SMC-RF
+#---Run ABC-SMC-RF
+abcrf_results <- smcrf(
+    method = "smcrf-single-param",
+    statistics_target = statistics_target,
+    model = model,
+    perturbation = "Uniform",
+    rprior = rprior,
+    dprior = dprior,
+    perturbation_parameters = perturbation(4),
+    ntree = 2500,
+    nParticles = rep(5000, 4),
+    save_model = FALSE,
+    parallel = TRUE,
+    model_redo_if_NA = TRUE
+)
+#---Compute m
+abcrf_results <- estimated_mean(abcrf_results)
+#---Plot marginal distributions against other methods
+plots_marginal <- plot_compare_marginal(
+    abc_results = abcrf_results,
+    parameters_labels = parameters_labels,
+    plot_hist = TRUE,
+    position_x = 0.9,
+    position_y = 0.5
+)
+#---Plot qqplots against other methods
+plots_compare_qqplot <- plot_compare_qqplot(
+    plots = plots_compare_qqplot,
+    abc_results = abcrf_results,
+    parameters_truth = parameters_truth,
+    parameters_labels = parameters_labels
+)
+# ===============================================================SMC-DRF
+#---Run SMC-DRF
+smcrf_results_multi_param <- smcrf(
+    method = "smcrf-multi-param",
+    statistics_target = statistics_target,
+    model = model,
+    rprior = rprior,
+    dprior = dprior,
+    perturbation = "Uniform",
+    perturbation_parameters = perturbation(4),
+    nParticles = rep(5000, 4),
+    save_model = FALSE,
+    num.trees = 2500,
+    splitting.rule = "FourierMMD",
+    model_redo_if_NA = TRUE,
+    parallel = TRUE
+)
+#---Compute m
+smcrf_results_multi_param <- estimated_mean(smcrf_results_multi_param)
+#---Plot posterior joint distributions against other methods
+plots_joint <- plot_compare_joint(
+    plots = plots_joint,
+    abc_results = smcrf_results_multi_param,
+    parameters_labels = parameters_labels
+)
+#---Plot qqplots
+plots_compare_qqplot <- plot_compare_qqplot(
+    plots = plots_compare_qqplot,
+    abc_results = smcrf_results_multi_param,
+    lims = data.frame(
+        parameter = c("kappa", paste0("prob_", 0:(length(prob_X) - 1)), "gamma"),
+        min = c(0, rep(0, length(prob_X)), 0),
+        max = c(10, rep(1, length(prob_X)), 1)
+    ),
+    parameters_truth = parameters_truth,
+    parameters_labels = parameters_labels
+)
+#---Plot marginal distributions against other methods
+plots_marginal <- plot_compare_marginal(
+    plots = plots_marginal,
+    abc_results = smcrf_results_multi_param,
+    parameters_labels = parameters_labels,
+    plot_hist = TRUE,
+    position_x = 0.9,
+    position_y = 0.1
+)
